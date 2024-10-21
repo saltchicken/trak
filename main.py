af53626d3696f527377638f712dbd7c81ceef0ea
saltchicken <john.eicher89@gmail.com>
2024-10-20 21:03:19 -0700

    Increase max rows

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import trak
 
-trak.pd.set_option("display.max_rows", 500)
+trak.pd.set_option("display.max_rows", 1000)
 df = trak.log_parser("/var/log/nginx/access.log")
 print(
     df[df["status_code"] == "200"].pivot_table(
