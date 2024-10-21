9f9037807e736fa806cc77aa02727818d640e4d1
saltchicken <john.eicher89@gmail.com>
2024-10-20 21:00:45 -0700

    Add main entry point for exporting a pandas dataframe of log

---
+++ main.py
@@ -0,0 +1,9 @@
+import trak
+
+trak.pd.set_option("display.max_rows", 500)
+df = trak.log_parser("/var/log/nginx/access.log")
+print(
+    df[df["status_code"] == "200"].pivot_table(
+        "ip", "url", "status_code", aggfunc="nunique"
+    )
+)
