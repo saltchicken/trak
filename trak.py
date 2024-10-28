34ac3d90e6dbbd1a181588bda06860f97e2341dc
saltchicken <john.eicher89@gmail.com>
2024-10-27 19:16:56 -0700

    Revert

--- trak.py
+++ trak.py
@@ -224,18 +224,10 @@ def insert_log_message_into_table(logs_df):
         )
 
 
-def insert_into_table(logs_df, append=False):
-    if append:
-        current_line = sql_cursor.query_size_of_log_messages_table()
-    else:
-        current_line = 0
-    unique_ips = logs_df["ip"].unique()
-    for line_number, ip in enumerate(unique_ips):
-        if line_number < current_line:
-            continue
-
+def insert_into_table(logs_df):
+    for ip in logs_df["ip"].unique():
         if sql_cursor.check_if_ip_exists(ip):
-            print(f"Record already exists for {ip}")
+            logger.info(f"Record already exists for {ip}")
         else:
             logger.info(f"Running GPS on {ip}")
             latitude, longitude = get_coordinates(ip)
@@ -273,7 +265,7 @@ if __name__ == "__main__":
         tail_f("/var/log/nginx/access.log")
 
     elif args.update_db:
-        logs_df = log_parser("/var/log/nginx/access.log", append=True)
+        logs_df = log_parser("/var/log/nginx/access.log")
         insert_into_table(logs_df)
 
     elif args.update_logs:
