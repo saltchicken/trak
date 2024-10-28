4c0f13e59a650c472d32b6a54ecab0ab1876975a
saltchicken <john.eicher89@gmail.com>
2024-10-27 19:30:06 -0700

    Add some debugging

--- trak.py
+++ trak.py
@@ -224,7 +224,11 @@ def insert_log_message_into_table(logs_df):
         )
 
 
-def insert_into_table(logs_df):
+def insert_into_table(logs_df, append=False):
+    if append:
+        print("append")
+    else:
+        print("no appaend")
     for ip in logs_df["ip"].unique():
         if sql_cursor.check_if_ip_exists(ip):
             logger.debug(f"Record already exists for {ip}")
